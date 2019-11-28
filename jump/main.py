97c23ffd53a8cd78bf422ca0ecc6fd9d7a6946a2
inamuu <8310973+inamuu@users.noreply.github.com>
2019-11-28 20:38:02 +0900

    :+1: day2: マウスで猫を動かす

--- jump/main.py
+++ jump/main.py
@@ -9,11 +9,16 @@ CAT_H = 16
 
 class App:
     def __init__(self):
-        pyxel.init(WINDOW_W, WINDOW_H, caption='Hello inamuu')
-        pyxel.image(0).load(0, 0, "pyxel_examples/assets/pyxel_logo_38x16.png")
-        pyxel.image(1).load(0, 0, "pyxel_examples/assets/cat_16x16.png")
+        self.IMG_ID0 = 0
+        self.IMG_ID1 = 1
+        self.IMG_ID0_X = 60
+        self.IMG_ID1_Y = 65
 
-        pyxel.mouse(True)
+        pyxel.init(WINDOW_W, WINDOW_H, caption='Hello inamuu')
+        pyxel.image(self.IMG_ID0).load(0, 0, "pyxel_examples/assets/pyxel_logo_38x16.png")
+        pyxel.image(self.IMG_ID1).load(0, 0, "pyxel_examples/assets/cat_16x16.png")
+
+        pyxel.mouse(False)
 
         pyxel.run(self.update, self.draw)
 
@@ -23,7 +28,16 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.blt(60, 65, 0, 0, 0, 38, 16)
-        pyxel.blt(75, 45, 1, 0, 0, CAT_W, CAT_H, 5)
+        pyxel.blt(self.IMG_ID0_X, self.IMG_ID1_Y, self.IMG_ID0, 0, 0, 38, 16)
+        self.cat()
+
+    def cat(self):
+        x = pyxel.mouse_x
+        y = pyxel.mouse_y
+
+        if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
+            pyxel.blt(x, y, self.IMG_ID1, 0, 0, -CAT_W, CAT_H, 5)
+        else:
+            pyxel.blt(x, y, self.IMG_ID1, 0, 0, CAT_W, CAT_H, 5)
 
 App()
